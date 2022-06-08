9fa99cd2b6ff213d617fab46580ca1d8816f8819
Etta Baker <etta.r.baker@gmail.com>
2022-06-08 14:23:58 -0400

    Updated api.js

--- src/api.js
+++ src/api.js
@@ -2,61 +2,6 @@ import { mockData } from "./mock-data";
 import axios from "axios";
 import NProgress from "nprogress";
 
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
-
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://52c4nxu9zd.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-
-  // if (!accessToken || tokenCheck.error) {
-  //   await localStorage.removeItem("access_token");
-  //   const searchParams = new URLSearchParams(window.location.search);
-  //   const code = await searchParams.get("code");
-  //   if(!code) {
-  //     const results = await axios.get(
-  //       "https://52c4nxu9zd.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-  //     );
-  //     const { authUrl } = results.data;
-  //     return (window.location.href = authUrl);
-  //   }
-  //   return code && getToken(code);
-  // }
-  // return accessToken;
-}
-
-const checkToken = async (accessToken) => {
-  const result = await fetch(
-    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-    )
-    .then((res) => res.json())
-    .catch((error) => error.json());
-  
-  return result;
-}
-
 const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
     var newurl = 
@@ -69,6 +14,30 @@ const removeQuery = () => {
     newurl = window.location.protocol + "//" + window.location.host;
     window.history.pushState("", "", newurl);
   }
+};
+
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+  
+  return result;
+};
+
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
+ export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
 };
 
 export const getEvents = async () => {
@@ -96,6 +65,26 @@ export const getEvents = async () => {
   }
 };
 
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://52c4nxu9zd.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
+
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
@@ -111,9 +100,5 @@ const getToken = async (code) => {
   return access_token;
 };
 
-export const extractLocations = (events) => {
-  var extractLocations = events.map((event) => event.location);
-  var locations = [...new Set(extractLocations)];
-  return locations;
-};
+
 
