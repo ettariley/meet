db79660fd83fadbe49e3121b77a55ab7db6dacca
Etta Baker <etta.r.baker@gmail.com>
2022-06-17 18:57:59 -0400

    Implemented events caching

--- src/api.js
+++ src/api.js
@@ -48,6 +48,13 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const { events } = await localStorage.getItem("lastEvents");
+    NProgress.done();
+    
+    return { events: JSON.parse(events), locations: extractLocations(events) };
+  }
+  
   const token = await getAccessToken();
   console.log(token);
 
