f0cba718a17d7396e1ed4914d7de29c5425e7c23
Etta Baker <etta.r.baker@gmail.com>
2022-06-17 22:04:51 -0400

    Added welcome screen & updated privacy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import WelcomeScreen from './WelcomeScreen';
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { OfflineAlert } from "./Alert";
 import './nprogress.css';
 
@@ -12,7 +13,8 @@ class App extends Component {
     events: [],
     locations: [],
     eventCount: null,
-    offlineText: ''
+    offlineText: '',
+    showWelcomeScreen: undefined
   }
 
   updateEvents = (location, eventCount) => {
@@ -50,20 +52,26 @@ class App extends Component {
     }
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      let firstEventList = events;
-      const eventCount = Math.min(firstEventList.length, 32);
-      if (this.mounted) {
-        this.setState({ 
-          events: firstEventList.slice(0, eventCount),
-          locations: extractLocations(events), 
-          eventCount: eventCount
-        });
-      }
-    });
-    
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        let firstEventList = events;
+        const eventCount = Math.min(firstEventList.length, 32);
+        if (this.mounted) {
+          this.setState({ 
+            events: firstEventList.slice(0, eventCount),
+            locations: extractLocations(events), 
+            eventCount: eventCount
+          });
+        }
+      }); 
+    }
   }
 
   componentWillUnmount() {
@@ -71,12 +79,14 @@ class App extends Component {
   }
  
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className='App' />
     return (
       <div className="App">
         <OfflineAlert text={this.state.offlineText} />
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents eventCount={this.state.eventCount} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   };
